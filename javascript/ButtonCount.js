8d291b995d64b6e43f3942b2b86c8909a1780aca
Samuel Liu <samuel.liu1004@gmail.com>
2023-03-09 12:16:04 -0800

    Style update

--- javascript/ButtonCount.js
+++ javascript/ButtonCount.js
@@ -3,26 +3,32 @@ class ButtonCount extends HTMLElement {
   constructor() {
     super();
 
-    const shadow = this.attachShadow({ mode: "open" });
+    // keeps track of how many times the button has been clicked
+    this.clickCount = 0;
 
+    // create actual HTML button that keeps track of how many times it is clicked
+    const shadow = this.attachShadow({ mode: "open" });
     const actualButton = document.createElement("button");
-    actualButton.innerText = "Current click count: 0";
+    actualButton.innerText = "Times Clicked: 0";
     shadow.appendChild(actualButton);
+  }
+
+  // update the button's text to reflect the updated click count
+  incrementCount() {
+    this.shadowRoot.firstChild.innerText = "Times Clicked: " + ++this.clickCount;
   }
 }
 
 // add custom element to custom elements registry
 customElements.define("button-count", ButtonCount);
 
+// initializes the onclick listener for the button-count element
 function initButton() {
-  var click_count = 0;
   const buttonCountElem = document.getElementById("my-btn-count");
-
-  buttonCountElem.addEventListener("click", () => {
-    click_count++;
-    const shadow = buttonCountElem.shadowRoot;
-    shadow.firstChild.innerText = "Current click count: " + click_count;
-  });
+  buttonCountElem.onclick = () => {
+    buttonCountElem.incrementCount();
+  };
 }
 
+// only initialize on click listeners after the DOM is loaded
 document.addEventListener("DOMContentLoaded", initButton);
