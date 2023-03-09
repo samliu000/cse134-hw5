1a0fbbd29e5a7b2959ec4a979f03200b7c32e4d4
Samuel Liu <samuel.liu1004@gmail.com>
2023-03-09 14:32:51 -0800

    separate into js modules

--- javascript/methodtest.js
+++ javascript/methodtest.js
@@ -1,86 +1,8 @@
-// get html elements
-const formElement = document.getElementById("method_test_form");
-const outputElement = document.getElementById("response");
-const postBtn = document.getElementById("postBtn");
-const dateField = document.getElementById("date_time");
-const fetchRequestType = document.getElementById("fetch_radio");
-const xhrRequestType = document.getElementById("xhr_radio");
-
-// use fetch or xhr api to make request
-function sendNetworkRequest(type) {
-  // use fetch api
-  if (fetchRequestType.checked) {
-    fetch(getRequestUrl(type), getFetchOptions(type))
-      .then((response) => response.json())
-      .then((data) => populateOutput(data))
-      .catch((error) => console.error(error));
-  }
-  // use XMLHttpRequest api
-  else if (xhrRequestType.checked) {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (this.readyState == XMLHttpRequest.DONE) {
-        if (this.status == 200) {
-          populateOutput(JSON.parse(this.responseText));
-        } else {
-          console.log("XHR Request Error: " + this.statusText);
-        }
-      }
-    };
-    xhr.open(type, getRequestUrl(type));
-    if (type != "get") {
-      xhr.send(getFormData());
-    } else {
-      xhr.send();
-    }
-  }
-}
-
-// populates output field with the returned json data
-function populateOutput(data) {
-  outputElement.innerHTML = `<h2>Request Response</h2><pre>${JSON.stringify(data, null, 4)}</pre>`;
-}
-
-// gets the request url, for get requests, the form data is added to the url
-function getRequestUrl(type) {
-  if (type == "post") {
-    return "https://httpbin.org/post";
-  } else if (type == "get") {
-    return "https://httpbin.org/get?" + getFormData().toString();
-  } else if (type == "put") {
-    return "https://httpbin.org/put";
-  } else if (type == "delete") {
-    return "https://httpbin.org/delete";
-  }
-}
-
-// create fetch options. for get, there are no options
-function getFetchOptions(type) {
-  if (type == "get") {
-    return {};
-  } else {
-    return {
-      method: type,
-      body: getFormData(),
-    };
-  }
-}
-
-// grabs all fields of the form and return it as a URLSearchParams object
-function getFormData() {
-  // insert current date
-  dateField.value = new Date();
-
-  // grab all fields of the form
-  const data = new URLSearchParams();
-  for (const pair of new FormData(formElement)) {
-    data.append(pair[0], pair[1]);
-  }
-  return data;
-}
+import { sendNetworkRequest } from "/javascript/methodtestNetworkHelpers.js";
 
 // adds event listeners for all the method buttons
 function initButtons() {
+  const postBtn = document.getElementById("postBtn");
   postBtn.addEventListener("click", () => {
     sendNetworkRequest("post");
   });
